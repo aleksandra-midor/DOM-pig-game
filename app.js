d055ca910e7d6f14a4368e97550f445b6de281e5
Aleksandra Midor <ola_midor@outlook.com>
2020-05-03 21:00:03 +0200

    additional rule on rolling 2 '6' support for changing end score value

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ GAME RULES:
 
 var scores, roundScore, activePlayer, gamePlaying;
 
+var lastDice;
+
 init();
+
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if(gamePlaying) {
@@ -25,7 +28,11 @@ diceDOM.src = 'dice-' + dice + '.png';
 
 
 // 3. Update the round score IF the rolled number was NOT a 1
-if (dice !== 1) {
+if (dice === 6 && lastDice === 6) {
+    scores[activePlayer] = 0;
+    document.querySelector('#scores-' + activePlayer).textContent = '0';
+    nextPlayer();
+} else if (dice !== 1) {
     // Add score
     roundScore += dice;
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
@@ -33,6 +40,9 @@ if (dice !== 1) {
     // Next player
     nextPlayer();
 }
+
+lastDice = dice;
+
     }
     
 });
@@ -40,15 +50,19 @@ if (dice !== 1) {
 document.querySelector('.btn-hold').addEventListener('click', function() {
     if(gamePlaying) {
       // Add current score to global score
-    scores[activePlayer] += roundScore;
+      scores[activePlayer] += roundScore;
+      
+      // Update the UI
+      document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+      
+      var input =  document.querySelector('.final-score').value;
+      console.log(input);
 
-    // Update the UI
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+      // Check if the player won the game
 
-    // Check if the player won the game
-    if (scores[activePlayer] >= 20) {
-        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-        document.querySelector('.dice').style.display = 'none';
+      if (scores[activePlayer] >= 20) {
+          document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+          document.querySelector('.dice').style.display = 'none';
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
         gamePlaying = false;
